17ba43ba836c74bb7efa2b278fcdc9e9aea62df2
Spoffy <spoffeh@gmail.com>
2014-12-21 10:59:18 +0000

    Restart script usable as library. Fully working.

--- restart_ss13.py
+++ restart_ss13.py
@@ -2,20 +2,38 @@
 
 import psutil
 import argparse
+import subprocess
 
-_PROCESS_NAME = "DreamDaemon"
+PROCESS_NAME = "DreamDaemon"
+DD_INSTALL_PATH = "/usr/local/byond/bin/DreamDaemon"
 
-parser = argparse.ArgumentParser(description="Restarts the DreamDaemon instance specified by the named '.dmb.'")
-parser.add_argument("dmb", metavar="*.dmb", help="The .dmb name of the server to restart")
-args = parser.parse_args()
 
 def running_dreamdaemons():
   return [process for process in psutil.process_iter() 
-          if process.name() == _PROCESS_NAME]
+          if process.name() == PROCESS_NAME]
 
-def is_daemon_running(daemon_dmb):
+def is_daemon_running(dmb_name):
   for daemon in running_dreamdaemons():
-    if args.dmb in daemon.cmdline(): return True
+    if dmb_name in daemon.cmdline(): return True
   return False
 
-print(is_daemon_running(args.dmb))
+def restart_server(dmb_name, dreamdaemon_args):
+  if not is_daemon_running(dmb_name):
+    dd_args = [DD_INSTALL_PATH, dmb_name] + dreamdaemon_args
+    process = subprocess.Popen(dd_args)
+    return process
+
+def main():
+  parser = argparse.ArgumentParser(description="Restarts the DreamDaemon instance specified by the named '.dmb.'")
+  parser.add_argument("dmb", metavar="*.dmb", help="The .dmb name of the server to restart")
+  parser.add_argument("dd_args", metavar="...", nargs=argparse.REMAINDER, help="The arguments to pass to DreamDaemon, excluding the .dmb")
+  args = parser.parse_args()
+
+  process = restart_server(args.dmb, args.dd_args)
+  if process.returncode == None or process.returncode == 0:
+    print("Command run successfully, PID: " + str(process.pid))
+  else:
+    print("Command failed with return code " + str(process.returncode))
+
+if __name__ == "__main__":
+  main()
