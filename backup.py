65780d42d6a979f691c2f23fd0ceb90970835289
Spoffy <spoffy@llagaming.net>
2014-12-22 13:24:48 +0000

    [Moved] Backup command from backup.py to main control script.

--- backup.py
+++ backup.py
@@ -3,7 +3,6 @@
 import os
 import os.path as path
 import shutil
-import argparse
 from time import gmtime, time, strftime
 
 def utc_timestamp():
@@ -20,18 +19,3 @@ def make_backup(files, dest):
   for src in files:
     shutil.copy(src, final_path)
   return True
-
-def _main():
-  parser = argparse.ArgumentParser(description="Backup a list of files to a directory.")
-  parser.add_argument("dest", help="Destination directory (Doesn't need to exist)")
-  parser.add_argument("files", metavar="file", help="File to copy", nargs="+")
-  args = parser.parse_args()
-
-  print("Beginning backup of {0} to {1}".format(str(args.files), str(args.dest)))
-  if make_backup(args.files, args.dest):
-    print("Backup successful")
-  else:
-    print("Backup failed, destination is probably a file")
-
-if __name__ == "__main__":
-  _main()
