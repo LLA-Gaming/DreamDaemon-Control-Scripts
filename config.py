5a9090e90a5fc2b8d53cf0f7b0354380ea286b50
Spoffy <spoffy@llagaming.net>
2014-12-22 18:20:18 +0000

    [Changed] Config file format

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ CONFIGS = [
   "name": "Test",
   "path": "/",
   "dmb": "teststation.dmb",
+  "port": "52600",
+  "args": ["-logself", "-trusted", "-unsafe_diag"],
   "backup_files": [],
   "backup_dest": "/"
 }
