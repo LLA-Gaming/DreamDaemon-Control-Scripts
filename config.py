1a2fd9aa64a83909613c120cf832e8931ef6a461
Spoffy <spoffeh@gmail.com>
2014-12-22 15:39:34 +0000

    [Changed] start command takes a config id number, helper function added to config.py

--- config.py
+++ config.py
@@ -1,13 +1,20 @@
+import os.path
+
 CONFIGS = [
 {
   "name": "Test",
   "path": "/",
   "dmb": "teststation.dmb",
-  "port": "52600",
+  "port": "52601",
   "args": ["-logself", "-trusted", "-unsafe_diag"],
-  "backup_files": [],
+  "backup_files": ["config/admins.txt"],
   "backup_dest": "/"
 }
 ]
 
-DEFAULT_CONFIG = CONFIGS[0]
+#Index of the default configuration to use.
+DEFAULT_CONFIG = 0
+
+
+def get_dmb_path(config):
+  return os.path.join(config["path"], config["dmb"])
--- dream_control.py
+++ dream_control.py
@@ -6,18 +6,18 @@ from os.path import basename
 from subprocess import call
 from daemon import *
 from backup import make_backup
-from config import CONFIGS, DEFAULT_CONFIG
+from config import CONFIGS, DEFAULT_CONFIG, get_dmb_path
 
 PATH_TO_CRONTAB = "/"
 
 def list_configs(args):
   count = 0
   for config in CONFIGS:
-    count += 1
     print("Config {0}:".format(count))
     for (key, value) in config.iteritems():
       print("    {0}: {1}".format(key, value))
     print("\n")
+    count += 1
 
 def list_daemons(args):
   daemons = running_dreamdaemons()
@@ -25,31 +25,30 @@ def list_daemons(args):
     print("Daemon: {0}".format(" ".join(daemon.cmdline())))
 
 def start_daemon(args):
+  config = CONFIGS[args.config]
   call(["crontab", PATH_TO_CRONTAB])
-  print("Starting daemon running {0}".format(args.dmb_path))
-  start_daemon_if_stopped(args.dmb_path, args.dd_args)
+  print("Starting daemon running {0}".format(config["dmb"]))
+  start_daemon_if_stopped(get_dmb_path(config), config["port"], config["args"])
 
 def start_default_daemon(args):
-  args.dmb_path = os.path.join(DEFAULT_CONFIG["path"], DEFAULT_CONFIG["dmb"])
-  args.dd_args = [DEFAULT_CONFIG["port"]] + DEFAULT_CONFIG["args"]
+  args.config = DEFAULT_CONFIG
   start_daemon(args)
 
-def stop_daemon(args, force=False):
+def stop_daemon(args):
   call(["crontab", "-r"])
   print("Stopping daemon running {0}".format(args.dmb_name))
-  daemon.stop_daemon(args.dmb_name, force)
+  daemon.stop_daemon(args.dmb_name, args.force)
 
 def stop_default_daemon(args):
-  args.dmb_name = DEFAULT_CONFIG["dmb"]
+  args.dmb_name = CONFIGS[DEFAULT_CONFIG]["dmb"]
   stop_daemon(args)
 
 def restart_default_daemon(args):
-  args.dmb_name = DEFAULT_CONFIG["dmb"]
-  stop_daemon(args.dmb_name, force=True)
+  stop_default_daemon(args)
   start_default_daemon(args)
 
 def edit_admins(args):
-  call(["nano", os.path.join(DEFAULT_CONFIG["path"], "config/admins.txt")])
+  call(["nano", os.path.join(CONFIG[DEFAULT_CONFIG]["path"], "config/admins.txt")])
 
 def backup(args):
   print("Beginning backup of {0} to {1}".format(str(args.files), str(args.dest)))
@@ -71,28 +70,29 @@ def _main():
   parser_list = subparsers.add_parser("list", help="Lists all running daemons")
   parser_list.set_defaults(func=list_daemons)
 
-  parser_start = subparsers.add_parser("start", help="Starts (if not currently running) the DreamDaemon instance specified by the named '.dmb.'")
-  parser_start.add_argument("dmb_path", metavar="/foo/bar/*.dmb", help="Path to the .dmb used by the DreamDaemon instance")
-  parser_start.add_argument("dd_args", metavar="...", nargs=argparse.REMAINDER, help="The arguments to pass to DreamDaemon, excluding the .dmb")
+  parser_start = subparsers.add_parser("start", help="Starts (if not currently running) the DreamDaemon instance specified by the config number.")
+  parser_start.add_argument("config", type=int, help="Config number of the chosen configuration, e.g 1, 2, 3.")
   parser_start.set_defaults(func=start_daemon)
 
   parser_stop = subparsers.add_parser("stop", help="Stops the DreamDaemon instance specified by the named '.dmb.'")
   parser_stop.add_argument("dmb_name", metavar="*.dmb", help="Name of the .dmb used by the DreamDaemon instance")
+  parser_stop.add_argument("-force", action="store_true", help="Sends SIGKILL instead of SIGTERM - Kills it outright")
   parser_stop.set_defaults(func=stop_daemon)
 
   parser_stop_default = subparsers.add_parser("stop_default", 
                                               help="""Stops the default daemon running and prevents autorestart.
-                                                      Currently configured to: """ + DEFAULT_CONFIG["path"])
+                                                      Currently configured to: """ + CONFIGS[DEFAULT_CONFIG]["path"])
+  parser_stop_default.add_argument("-force", action="store_true", help="Sends SIGKILL instead of SIGTERM - Kills it outright")
   parser_stop_default.set_defaults(func=stop_default_daemon)
 
   parser_restart_default = subparsers.add_parser("restart_default",
                                                  help="""Immediately kills (SIGKILL) the default daemon and starts
-                                                         it again. Currently configured to: """ + DEFAULT_CONFIG["path"])
+                                                         it again. Currently configured to: """ + CONFIGS[DEFAULT_CONFIG]["path"])
   parser_restart_default.set_defaults(func=restart_default_daemon)
 
   parser_start_default = subparsers.add_parser("start_default",
                                                help="""Starts the default daemon.
-                                                       Currently configured to: """ + DEFAULT_CONFIG["path"])
+                                                       Currently configured to: """ + CONFIGS[DEFAULT_CONFIG]["path"])
   parser_start_default.set_defaults(func=start_default_daemon)
 
   parser_edit_admins = subparsers.add_parser("edit_admins", help="Opens admins.txt in nano.")
